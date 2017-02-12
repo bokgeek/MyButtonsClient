798d44fe2dfdf848acf54d56faa6900c82d54e5a
BoK Geek <bok.geek@gmail.com>
2017-02-12 21:04:36 +0100

    fix splashscreen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,17 +6,26 @@ import { TabsPageComponent } from '../pages/tabs/tabs';
 
 
 @Component({
-  templateUrl: 'app.html'
+    templateUrl: 'app.html'
 })
 export class MyButtonsComponent {
-  rootPage = TabsPageComponent;
+    rootPage = TabsPageComponent;
 
-  constructor(platform: Platform) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      StatusBar.styleDefault();
-      Splashscreen.hide();
-    });
-  }
+    constructor(platform: Platform) {
+        platform.ready().then(() => {
+            // Okay, so the platform is ready and our plugins are available.
+            // Here you can do any higher level native things you might need.
+            StatusBar.styleDefault();
+            // Splashscreen.hide();
+            this.hideSplashScreen();
+        });
+    }
+
+    private hideSplashScreen() {
+        if (Splashscreen) {
+            setTimeout(() => {
+                Splashscreen.hide();
+            }, 100);
+        }
+    }
 }
